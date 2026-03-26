9e14c8dfd78c50836f30b7949f8b0d0f2302455b
boog1108 <boogyjam@gmail.com>
2026-03-26 16:22:43 +0100

    started project

---
+++ src/main/java/dev/boog/money_tracker_api_gateway/MoneyTrackerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.boog.money_tracker_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackerApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTrackerApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/boog/money_tracker_api_gateway/MoneyTrackerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.boog.money_tracker_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MoneyTrackerApiGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
